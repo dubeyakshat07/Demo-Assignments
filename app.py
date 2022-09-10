b83daebd4c20ac0f22a646fec33a92e2d6f5d4f1
Akshat Dubey <47806749+dubeyakshat07@users.noreply.github.com>
2022-09-10 11:26:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.graph_objs as go
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server=app.server
 # colors = {
 #     'background': '#fff',
 #     # 'text': '#7FDBFF'
